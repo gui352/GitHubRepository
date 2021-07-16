cfa41cef8dc87a1c09f053df3b83fdf82c3545c5
Linzmayer <lintzguigui1323@gmail.com>
2021-07-16 15:25:12 -0300

    tag Link e salvamento em memória cash 16-07

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
-import { useState, FormEvent } from 'react';
-import { Title, Repositories, Form } from './styles';
+import { useState, useEffect, FormEvent } from 'react';
+import { Title, Repositories, Form, Error } from './styles';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
-import { FiChevronRight } from 'react-icons/fi';
 
 interface Repository{
   full_name: string;
@@ -14,31 +14,63 @@ interface Repository{
 
 const Dashboard: React.FC = () => {
   const[newRepo, setNewRepo] = useState('');
-  const[repositories, setRepositories] = useState<Repository[]>([]);
+  const [inputError, setInputError] = useState('');
+  const[repositories, setRepositories] = useState<Repository[]>(() => {
+    const storageRepository = localStorage.getItem(
+      '@GitHubExplorer:repositories'
+    );
+
+    if(storageRepository){
+      return JSON.parse(storageRepository);
+    }
+
+    return[];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GitHubExplorer:repositories',
+      JSON.stringify(repositories)
+    )
+  }, [repositories]);
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void>{
       event.preventDefault();
 
-      const response = await api.get<Repository>(`repos/${newRepo}`);
-      const repository = response.data;
+      if(!newRepo){
+        setInputError("Digite um usuário/repositório para pesquisar. ");
+        return;
+      }
 
-      setRepositories([...repositories, repository]);
-      setNewRepo('');
+      try{
+
+        const response = await api.get<Repository>(`repos/${newRepo}`);
+        const repository = response.data;
+
+        setRepositories([...repositories, repository]);
+        setNewRepo('');
+
+      }catch(err){
+        setInputError("Repositório não encontrado ou inexistente. ");
+
+      }
   }
 
   return (
     <>
       <Title>Explore repositórios no GitHub</Title>
 
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={Boolean(inputError)} onSubmit={handleAddRepository}>
         <input onChange={e => setNewRepo(e.target.value)} placeholder="Digite o nome do repositório" value={newRepo}/>
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError }</Error>}
+
       <Repositories>
 
-        {/**repositories.map(repository => (
-        <a key={repository.full_name} href="teste">
+        {repositories.map(repository => (
+        <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -47,56 +79,8 @@ const Dashboard: React.FC = () => {
               <strong>{repository.full_name}</strong>
               <p>{repository.description}</p>
             </div>
-        </a>
-        ))**/}
-
-        <a href="teste">
-            <img
-              src="https://avatars.githubusercontent.com/u/80405961?v=4"
-              alt="Guilherme"
-            />
-            <div>
-              <strong>ProjetoLoja</strong>
-              <p>Sistema de cadastro, edição, exclusão e listagem de produtos. Desenvolvido em Java</p>
-            </div>
-            <FiChevronRight size={20}/>
-        </a>
-
-        <a href="teste">
-            <img
-              src="https://avatars.githubusercontent.com/u/80405961?v=4"
-              alt="Guilherme"
-            />
-            <div>
-              <strong>ProjetoLoja</strong>
-              <p>Sistema de cadastro, edição, exclusão e listagem de produtos. Desenvolvido em Java</p>
-            </div>
-            <FiChevronRight size={20}/>
-        </a>
-
-        <a href="teste">
-            <img
-              src="https://avatars.githubusercontent.com/u/80405961?v=4"
-              alt="Guilherme"
-            />
-            <div>
-              <strong>ProjetoLoja</strong>
-              <p>Sistema de cadastro, edição, exclusão e listagem de produtos. Desenvolvido em Java</p>
-            </div>
-            <FiChevronRight size={20}/>
-        </a>
-
-        <a href="teste">
-            <img
-              src="https://avatars.githubusercontent.com/u/80405961?v=4"
-              alt="Guilherme"
-            />
-            <div>
-              <strong>ProjetoLoja</strong>
-              <p>Sistema de cadastro, edição, exclusão e listagem de produtos. Desenvolvido em Java</p>
-            </div>
-            <FiChevronRight size={20}/>
-        </a>
+        </Link>
+        ))}
 
       </Repositories>
     </>
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
+import { css } from 'styled-components';
+
+interface FormProps{
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -8,19 +13,24 @@ export const Title = styled.h1`
   line-height: 56px;
   margin-top: 80px;
 `;
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
 
   input {
     flex: 1;
-    height: 50px;
+    height: 46px;
     padding: 0 24px;
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
     font-size: 16px;
+
+    border: 2px solid #fff;
+    ${props => props.hasError && css`
+      border-color: #c53030;
+    `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -43,6 +53,13 @@ export const Form = styled.form`
     }
   }
 `;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin_top: 8px;
+`;
+
 export const Repositories = styled.div`
   margin-top: 40px;
   max-width:700px;
