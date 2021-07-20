a402ff749da8d415f135748883585599d8f2d6ed
Linzmayer <lintzguigui1323@gmail.com>
2021-07-20 13:56:39 -0300

    atualizações 20-07

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom'
+import { useRouteMatch, Link } from 'react-router-dom';
+
+import { Header, RepositoryInfo } from './styles';
+import LogoImg from '../../assets/logo.svg';
 
 interface RepositoryParams{
   repository: string;
@@ -8,7 +11,38 @@ interface RepositoryParams{
 const Repository: React.FC = () =>{
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+          <img src={LogoImg} alt="GitHubExplorer"/>
+          <Link to='/'>Voltar</Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars.githubusercontent.com/u/69631?s=88&v=4" alt="Facebook" />
+          <div>
+            <strong>Facebook</strong>
+            <p>Repositório do ReactJS no GitHub do Facebook</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>172K</strong>
+            <span>Starts</span>
+          </li>
+          <li>
+            <strong>34K</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>574</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+    </>
+  )
 };
 
 export default Repository;
